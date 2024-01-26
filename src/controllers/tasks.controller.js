5d74000725fc92aa58c84d3361a98123628fff52
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-26 00:58:21 -0300

    The query is created to create task and error handling

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,3 +1,5 @@
+import { pool } from "../db.js";
+
 export const getTasks = (req, res) => {
     res.send("Obteniendo tareas");
 };
@@ -6,8 +8,27 @@ export const getTask = (req, res) => {
     res.send("Obteniendo una tarea unica");
 };
 
-export const createTask = (req, res) => {
-    res.send("creando tarea");
+export const  createTask = async (req, res, next) => {
+    const { title, description } = req.body;
+    //db insert
+   try{
+
+    const result = await pool.query(
+        "INSERT INTO task(title, description) VALUES($1, $2) RETURNING *",  
+    [title, description]
+    );
+
+    //se puede insertar otras querys a partir de aqui 
+
+   res.json(result.rows[0]);
+   }catch(error){
+    if (error.code === "23505"){
+        return res.send("Ya existe una tarea con ese nombre");
+    }
+
+    next(error);
+   }
+     
 };
 
 export const updateTask = (req, res) => {
