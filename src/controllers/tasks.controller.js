b40655327a66f96ef14f54f9cc55606664e86b67
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-26 01:32:59 -0300

    The task request is created to the database and error handling

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,41 +1,41 @@
 import { pool } from "../db.js";
 
-export const getTasks = (req, res) => {
-    res.send("Obteniendo tareas");
+export const getAllTasks = async (req, res) => {
+  const result = await pool.query("SELECT * FROM task");
+  return res.json(result.rows);
 };
 
 export const getTask = (req, res) => {
-    res.send("Obteniendo una tarea unica");
+  res.send("Obteniendo una tarea unica");
 };
 
-export const  createTask = async (req, res, next) => {
-    const { title, description } = req.body;
-    //db insert
-   try{
-
+export const createTask = async (req, res, next) => {
+  const { title, description } = req.body;
+  //db insert
+  try {
     const result = await pool.query(
-        "INSERT INTO task(title, description) VALUES($1, $2) RETURNING *",  
-    [title, description]
+      "INSERT INTO task(title, description) VALUES($1, $2) RETURNING *",
+      [title, description]
     );
 
-    //se puede insertar otras querys a partir de aqui 
+    //se puede insertar otras querys a partir de aqui
 
-   res.json(result.rows[0]);
-   }catch(error){
-    if (error.code === "23505"){
-        return res.send("Ya existe una tarea con ese nombre");
+    res.json(result.rows[0]);
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.status(409).json({
+        message: "Ya existe una tarea con ese titulo",
+      });
     }
 
     next(error);
-   }
-     
+  }
 };
 
 export const updateTask = (req, res) => {
-    res.send("Actualizando tarea unica");
+  res.send("Actualizando tarea unica");
 };
 
 export const deleteTask = (req, res) => {
-    res.send("Eliminando tarea");
-}
-
+  res.send("Eliminando tarea");
+};
