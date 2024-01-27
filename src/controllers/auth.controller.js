73dd11695813b1eb544db6e3185519e9fc6ca7c4
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-26 21:08:47 -0300

    "post" query is created for signup in the database + error handling

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,34 @@
+import bcrypt from "bcrypt";
+import { pool } from "../db.js";
+
 export const signin = (req, res) => {
   res.send("Iniciando sesion");
 };
 
-export const signup = (req, res) => {
-  res.send("Registrando usuario");
+//sign up
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    //encriptar la contraseña
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    //insertar usuario
+    const result = await pool.query(
+      "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *",
+      [name, email, hashedPassword]
+    );
+    console.log(result);
+
+    return res.json(result.rows[0]);
+  } catch (error) {
+    if (error.code === "23505") {
+      return res.status(409).json({
+        message: "Ya existe un usuario con ese correo",
+      });
+    }
+  }
 };
 
 export const signout = (req, res) => {
@@ -11,5 +36,5 @@ export const signout = (req, res) => {
 };
 
 export const profile = (req, res) => {
-    res.send("Perfil de usuario");
-}
+  res.send("Perfil de usuario");
+};
