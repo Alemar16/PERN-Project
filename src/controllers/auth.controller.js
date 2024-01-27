9d7bbcb2b6ae21b1cefa7dccc6cbe9d00bc66a18
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-26 23:16:10 -0300

    feat: implement user registration with bcrypt, pool, and JWT
    This commit adds the user registration functionality to the 'signup' file. It utilizes 'bcrypt' for securely hashing passwords, 'pool' for seamless database interaction, and 'createAccessToken' from the 'jwt' library to generate access tokens. Additionally, it includes error handling, addressing cases such as duplicate email with the '23505' code.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import { pool } from "../db.js";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const signin = (req, res) => {
   res.send("Iniciando sesion");
@@ -12,16 +13,22 @@ export const signup = async (req, res) => {
   try {
     //encriptar la contraseña
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
 
     //insertar usuario
     const result = await pool.query(
       "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *",
       [name, email, hashedPassword]
     );
-    console.log(result);
 
-    return res.json(result.rows[0]);
+    //generar token
+    const token = await createAccessToken({ id: result.rows[0].id });
+
+    //return res.json(result.rows[0]);
+    return res.json({
+      token: token,
+    });
+
+    //manejo de errores
   } catch (error) {
     if (error.code === "23505") {
       return res.status(409).json({
