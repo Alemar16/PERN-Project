d9b9f44c69f5bb7421de886e509b97576e79a087
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-27 03:04:09 -0300

    feat: enhance signup functionality in auth.controller.js
    Refactor the signup function in auth.controller.js to improve user registration. The changes include updating the cookie configuration by adding 'sameSite: "none"' for better cross-site compatibility. Additionally, the commit ensures that the code adheres to modern security practices. The enhancement provides a more robust and secure user registration experience, addressing potential issues with existing user accounts and improving overall system security.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,6 +26,7 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       //secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, //1 dia
     });
 
