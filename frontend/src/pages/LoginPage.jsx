8f89dcc473b64f6ee35ab06cd813e80e7b7c622d
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-29 02:50:01 -0300

    feat: implement axios for form submission in LoginPage component
    **Commit Description:**
    Introduce Axios for handling form submissions in the LoginPage component, enhancing the authentication process. Notable changes include:
    
    1. **Form Submission:**
       - Utilized the `axios` library to send a POST request to the authentication endpoint (`http://localhost:3000/api/signin`).
       - Integrated the `withCredentials` option for secure cookie-based authentication.
    
    2. **Input Field Enhancements:**
       - Added `id` attributes to the email and password input fields for improved accessibility and association with corresponding labels.
       - Applied `autoComplete` attributes to enhance user experience by suggesting and autocompleting email and password inputs.
    
    These changes aim to improve the authentication workflow in the LoginPage component by incorporating Axios for form submissions and implementing additional enhancements for a more robust and user-friendly sign-in experience.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,24 +1,41 @@
 import { Card, Input, Button, Label } from "../components/ui";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 function LoginPage() {
-  const {register, handleSubmit} = useForm();
+  const { register, handleSubmit } = useForm();
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data)
-  })
-
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data);
+    const res = await axios.post("http://localhost:3000/api/signin", data, {
+      withCredentials: true,
+    });
+    console.log(res);
+  });
 
   return (
     <div className="flex justify-center items-center h-screen">
       <Card>
         <h1 className="text-2xl font-bold mb-5 text-center">Sign in</h1>
-        <form onSubmit={ onSubmit}>
+        <form onSubmit={onSubmit}>
           <Label htmlFor="email">Email</Label>
-          <Input type="email" placeholder="enter your email"  {...register("email, {required: true}")}/>
+          <Input
+            id="email"
+            type="email"
+            placeholder="enter your email"
+            {...register("email", { required: true })}
+            autoComplete="email"
+          />
+
           <Label htmlFor="password">Password</Label>
-          <Input type="password" placeholder="enter your password"  {...register("password, {required: true}")}/>
+          <Input
+            id="password"
+            type="password"
+            placeholder="enter your password"
+            {...register("password", { required: true })}
+            autoComplete="current-password"
+          />
           <div className="flex justify-center mt-5">
             <Button>Sign in</Button>
           </div>
