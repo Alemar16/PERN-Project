b794ed208a1b90eb305c36c60eff3e05683c586e
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-29 02:09:08 -0300

    style: refine styling in LoginPage component
    **Commit Description:**
    Enhance the visual presentation and styling in the LoginPage component for a more polished and user-friendly appearance. Key style updates include:
    
    1. **Form Input Styling:**
       - Utilized the `react-hook-form` library to manage form state and validations efficiently.
       - Applied dynamic styling to form inputs, ensuring a consistent and responsive look.
    
    2. **Form Labels:**
       - Incorporated `<Label>` components to improve form element identification.
       - Utilized the `htmlFor` prop in labels to associate them with the corresponding input fields.
    
    3. **Button Alignment:**
       - Centered the sign-in button for a more balanced layout (`flex justify-center mt-5`).
    
    4. **Link Styling:**
       - Improved the styling of the "Register" link with a smooth transition effect for a more engaging user experience.
    
    These style enhancements aim to provide a more cohesive and visually appealing LoginPage, contributing to a positive user interaction with the sign-in form.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,25 +1,34 @@
 import { Card, Input, Button, Label } from "../components/ui";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function LoginPage() {
+  const {register, handleSubmit} = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
+
   return (
     <div className="flex justify-center items-center h-screen">
       <Card>
         <h1 className="text-2xl font-bold mb-5 text-center">Sign in</h1>
-        <form>
+        <form onSubmit={ onSubmit}>
           <Label htmlFor="email">Email</Label>
-          <Input type="email" placeholder="enter your email" />
+          <Input type="email" placeholder="enter your email"  {...register("email, {required: true}")}/>
           <Label htmlFor="password">Password</Label>
-          <Input type="password" placeholder="enter your password" />
-          <div className="flex justify-end mt-5"> {/* Alinea el botón a la derecha */}
+          <Input type="password" placeholder="enter your password"  {...register("password, {required: true}")}/>
+          <div className="flex justify-center mt-5">
             <Button>Sign in</Button>
           </div>
 
-          <div className="flex flex-col items-center mt-5"> {/* Contenedor centrado verticalmente */}
-            <p className="text-center mb-2">
-              Don&apos;t have an account?{" "}
-            </p>
-            <Link to="/register" className="text-blue-500 transition duration-300 hover:text-blue-700">
+          <div className="text-center mt-5">
+            <p className="mb-2">Don&apos;t have an account?</p>
+            <Link
+              to="/register"
+              className="text-blue-500 mx-auto transition duration-300 hover:text-blue-700"
+            >
               Register
             </Link>
           </div>
