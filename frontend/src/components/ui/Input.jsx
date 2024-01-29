37e402e007ad543eadaca79b4cd39311a7b8805a
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-28 23:21:27 -0300

    Introduce a reusable Input component using `forwardRef` for form fields. This component accepts all standard input props and allows for customization. It is designed to work seamlessly with react-hook-form. The styles are configured to match the Zinc theme, ensuring consistency across the application.

---
+++ frontend/src/components/ui/Input.jsx
@@ -0,0 +1,16 @@
+import { forwardRef } from "react";
+
+export const Input = forwardRef((props, ref) => {
+  return (
+    <input
+      type="text"
+      className="bg-zinc-600 px-3 py-2 block my-2 w-full shadow-md rounded"
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
+Input.displayName = "Input";
+
+export default Input;
