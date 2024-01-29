f98431a64fe52c61a4ef7576970616b545580265
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-29 01:31:36 -0300

    feat: create reusable Label component for identity labeling
    **Commit Description:**
    Introduce a new component named Label, designed for identity labeling within various pages and components. The Label component utilizes the `children` prop to dynamically include content within the label, making it versatile for different use cases.
    
    The `htmlFor` prop is employed to associate the label with a specific form element using the corresponding `id`. This facilitates accessibility by ensuring that clicking the label focuses on the associated input field, enhancing user experience and adhering to web accessibility standards.
    
    Additionally, PropTypes are implemented to enforce the expected prop types, ensuring that the `children` prop is of type `node` and the `htmlFor` prop is a required string. This commit aims to provide a standardized and reusable Label component for consistent identity labeling across the application.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,18 +1,32 @@
-import {Card, Input, Button} from "../components/ui";
+import { Card, Input, Button, Label } from "../components/ui";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
   return (
     <div className="flex justify-center items-center h-screen">
       <Card>
-     <h1 className="text-2xl font-bold">Sign in</h1>
-     <form>
-      <Input type="email" placeholder="enter your email"/>
-      <Input type="password" placeholder="enter your password"/>
-      <Button>Sign in</Button>
-     </form>
+        <h1 className="text-2xl font-bold mb-5 text-center">Sign in</h1>
+        <form>
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" placeholder="enter your email" />
+          <Label htmlFor="password">Password</Label>
+          <Input type="password" placeholder="enter your password" />
+          <div className="flex justify-end mt-5"> {/* Alinea el botón a la derecha */}
+            <Button>Sign in</Button>
+          </div>
+
+          <div className="flex flex-col items-center mt-5"> {/* Contenedor centrado verticalmente */}
+            <p className="text-center mb-2">
+              Don&apos;t have an account?{" "}
+            </p>
+            <Link to="/register" className="text-blue-500 transition duration-300 hover:text-blue-700">
+              Register
+            </Link>
+          </div>
+        </form>
       </Card>
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
