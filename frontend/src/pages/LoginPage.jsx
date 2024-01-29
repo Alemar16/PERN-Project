436ecd8b5eb60758020e0dbf6a36e99e8372b630
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-29 00:48:31 -0300

    feat: add NotFound component and update routing in App.jsx
    **Commit Description:**
    Introduce the NotFound component (NotFound.jsx), a visually appealing representation for 404 errors. The component includes a title, a 404 message, and a button to navigate back to the home page. This addition enhances the user experience when encountering non-existent routes.
    
    In App.jsx, update the routing configuration to include the new NotFound component. A wildcard route (`<Route path="*" element={<NotFound />} />`) is added at the end of the Routes to catch any unmatched paths, ensuring that users are directed to the NotFound component when navigating to non-existing routes. This improves the application's navigation and error handling, providing a consistent user interface.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,16 @@
-
+import {Card, Input, Button} from "../components/ui";
 
 function LoginPage() {
   return (
-    <div>
-      Login page
+    <div className="flex justify-center items-center h-screen">
+      <Card>
+     <h1 className="text-2xl font-bold">Sign in</h1>
+     <form>
+      <Input type="email" placeholder="enter your email"/>
+      <Input type="password" placeholder="enter your password"/>
+      <Button>Sign in</Button>
+     </form>
+      </Card>
     </div>
   )
 }
