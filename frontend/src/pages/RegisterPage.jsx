1fd8ebda26022e606f2f3a33b48107f3df6caefd
Armando Martinez <alemar.martinez16@gmail.com>
2024-01-28 23:20:08 -0300

    Integrate the react-hook-form library in RegisterPage to manage and validate form state. Utilize the Input component for the name input field, setting its placeholder to "enter your name". This commit establishes the foundation for handling form data in the RegisterPage component.

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,11 +1,48 @@
-
+import { Input, Card, Button } from "../components/ui";
+import { useForm } from "react-hook-form";
 
 function RegisterPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
-    <div>
-      Register page
+    <div className="flex justify-center items-center h-screen">
+      <Card>
+        <h3 className="text-2xl font-bold">Register page</h3>
+
+        <form onSubmit={onSubmit}>
+          <Input
+            placeholder="enter your name"
+            {...register("name", { required: true })}
+          />
+          {errors.name && <p className="text-red-500">Name is required</p>}
+          <Input
+            type="email"
+            placeholder="enter your email"
+            {...register("email", { required: true })}
+          />
+          {errors.email && <p className="text-red-500">Email is required</p>}
+          <Input
+            type="password"
+            placeholder="enter your password"
+            {...register("password", { required: true })}
+          />
+          {errors.password && (
+            <p className="text-red-500">Password is required</p>
+          )}
+
+          <Button>Register</Button>
+        </form>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
